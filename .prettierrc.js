801488ed4d093a629386b8fdb5a2285d3cdfc8b8
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-17 19:55:35 +0200

    install tailwind + add eslint prettier tailwind config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    tabWidth: 2,
+    useTabs: false,
+    printWidth: 80,
+    endOfLine: 'auto',
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'always'
+}
